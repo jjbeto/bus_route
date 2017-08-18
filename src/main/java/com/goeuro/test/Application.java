9a39d8d692104ad3f91d25ca7b6d9b24ae19c4c7
betofonseca <jjbeto@gmail.com>
2017-08-18 16:58:54 -0300

    renaming default files

--- src/main/java/com/goeuro/test/Application.java
+++ src/main/java/com/goeuro/test/Application.java
@@ -1,12 +1,13 @@
-package com.goeuro.test.challenge;
+package com.goeuro.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChallengeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
+
 }
--- src/test/java/com/goeuro/test/ApplicationTests.java
+++ src/test/java/com/goeuro/test/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.goeuro.test.challenge;
+package com.goeuro.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ChallengeApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
