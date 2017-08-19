fa45ffb3a39a89611ac1f10678fa06650063856b
betofonseca <jjbeto@gmail.com>
2017-08-18 21:03:50 -0300

    tests for REST controller connecting to the service layer

--- src/main/java/com/goeuro/test/controller/ConsultController.java
+++ src/main/java/com/goeuro/test/controller/ConsultController.java
@@ -1,6 +1,8 @@
 package com.goeuro.test.controller;
 
 import com.goeuro.test.dto.ResultDto;
+import com.goeuro.test.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api")
 public class ConsultController {
 
+    @Autowired
+    private RouteService routeService;
+
     @RequestMapping(value = "/direct", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public ResultDto direct(@RequestParam(value = "dep_sid", required = true) String departure, //
-                            @RequestParam(value = "arr_sid", required = true) String arrival) {
-        return new ResultDto();
+    public ResultDto direct(@RequestParam(value = "dep_sid", required = true) Integer departure, //
+                            @RequestParam(value = "arr_sid", required = true) Integer arrival) {
+        return routeService.checkStations(departure, arrival);
     }
 
 }
--- src/test/java/com/goeuro/test/controller/ConsultControllerTest.java
+++ src/test/java/com/goeuro/test/controller/ConsultControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasToString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -30,9 +31,22 @@ public class ConsultControllerTest {
     }
 
     @Test
-    public void testLoadContext() throws Exception {
+    public void testFound() throws Exception {
         mockMvc.perform(get("/api/direct?dep_sid=3&arr_sid=6"))//
                 .andExpect(MockMvcResultMatchers.status().isOk())//
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dep_sid", hasToString("3")))//
+                .andExpect(MockMvcResultMatchers.jsonPath("$.arr_sid", hasToString("6")))//
+                .andExpect(MockMvcResultMatchers.jsonPath("$.direct_bus_route", hasToString("true")))//
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void testNotFound() throws Exception {
+        mockMvc.perform(get("/api/direct?dep_sid=3&arr_sid=8"))//
+                .andExpect(MockMvcResultMatchers.status().isOk())//
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dep_sid", hasToString("3")))//
+                .andExpect(MockMvcResultMatchers.jsonPath("$.arr_sid", hasToString("8")))//
+                .andExpect(MockMvcResultMatchers.jsonPath("$.direct_bus_route", hasToString("false")))//
                 .andDo(MockMvcResultHandlers.print());
     }
 
